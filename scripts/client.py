3f564b78c2b560077e111a01dc53d3970eae65f4
Tian-Nian <1250576969@qq.com>
2025-05-15 17:52:31 +0800

    add clent and server supprt, untested

---
+++ scripts/client.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('./')
+
+from utils.pickle_sender import Sender
+from utils.pickle_reciever import Reciever
+
+import socket
+import time
+import numpy as np
+
+def transform_data(data):
+    state = np.aray([data["left_arm"]["joint"], data["left_arm"]["gripper"], data["right_arm"]["joint"], data["right_arm"]["gripper"]])
+    img_arr = data["cam_head"]["color"], data["cam_right_wrist"]["color"], data["cam_left_wrist"]["color"]
+    return img_arr, state
+
+class client:
+    def __init__(self,robot,cntrol_freq=10):
+        self.robot = robot
+        self.cntrol_freq = cntrol_freq
+    
+    def set_up(self, server_ip, server_port, recievee_ip, reciever_port):
+        self.sender = Sender(server_ip, server_port)
+        self.reciever = Reciever(recievee_ip, reciever_port,self.robot.move())       
+
+        self.receiver.start()
+
+def move(self, message):
+    action_chunk = message["action_chunk"]
+    action_chunk = np.array(action_chunk)
+
+    for action in action_chunk:
+        left_action = action[:7]
+        right_action = action[-7:]
+        move_data = {
+            "left_arm":left_action,
+            "right_arm":ight_action,
+        }
+        self.robot.move()
+
+    def play_once(self):
+        raw_data = self.robot.get()
+        img_arr, state = transform_data(raw_data)
+        data_send = {
+            "img_arr",img_arr,
+            "state",state
+        }
+        # 发送数据
+        self.sender.send(data_send)
+        time.sleep(1 / self.cntrol_freq)
+    
+    def close(self):
+        self.sender.close()
+        self.receiver.close()
--- scripts/server.py
+++ scripts/server.py
@@ -1,43 +1,29 @@
+import sys
+sys.path.append('./')
+
+from utils.pickle_sender import Sender
+from utils.pickle_reciever import Reciever
+
 import socket
-import pickle
-import cv2
+import time
 
-def get_robot_state():
-    # 模拟函数，实际可替换成真实获取状态的代码
-    return {"joint_angles": [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]}
+class server:
+    def __init__(self,model,cntrol_freq=10):
+        self.cntrol_freq = cntrol_freq
+        self.model = model
+    
+    def set_up(self, server_ip, server_port, recievee_ip, reciever_port):
+        self.sender = Sender(server_ip, server_port)
+        self.reciever = Reciever(recievee_ip, reciever_port,self.infer())       
 
-def send_data_and_receive_action(server_ip, port):
-    # 建立 socket 连接
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((server_ip, port))
-
-    # 获取图像和状态
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
-    cap.release()
-
-    if not ret:
-        raise RuntimeError("Failed to capture image")
-
-    robot_state = get_robot_state()
-
-    # 打包数据
-    data = pickle.dumps({
-        "image": frame,
-        "state": robot_state
-    })
-
-    # 发送数据长度和数据
-    client.sendall(len(data).to_bytes(8, byteorder='big'))
-    client.sendall(data)
-
-    # 接收返回动作
-    action_len = int.from_bytes(client.recv(8), byteorder='big')
-    action_data = b''
-    while len(action_data) < action_len:
-        action_data += client.recv(action_len - len(action_data))
-
-    action = pickle.loads(action_data)
-    print("Received action:", action)
-
-    client.close()
+        self.receiver.start()
+    
+    def infer(self, message):
+        img_arr, state = message["img_arr"], message["state"]
+        self.model.update_observation_windows(img_arr, state)
+        action_chunk = self.model.get_action()
+        self.sender.send({"action_chunk":action_chunk})
+    
+    def close(self):
+        self.sender.close()
+        self.receiver.close()
