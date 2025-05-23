64cd05df46248c29fea42187dd20d1d902a2956d
Tian-Nian <1250576969@qq.com>
2025-05-23 19:07:14 +0800

    add curobo IK

--- planner/curobo_planner.py
+++ planner/curobo_planner.py
@@ -1,16 +1,22 @@
-# ********************** CuroboPlanner (optional) **********************
 from curobo.types.math import Pose as CuroboPose
 import time
-from curobo.types.robot import JointState
-from curobo.wrap.reacher.motion_gen import MotionGen, MotionGenConfig, MotionGenPlanConfig, PoseCostMetric
-from curobo.util import logger
 import torch
 import yaml
+from curobo.types.robot import JointState
+from curobo.wrap.reacher.motion_gen import MotionGen, MotionGenConfig, MotionGenPlanConfig, PoseCostMetric
+# cuRobo
+from curobo.types.base import TensorDeviceType
+from curobo.types.robot import RobotConfig
+from curobo.util_file import get_robot_configs_path, join_path, load_yaml
+from curobo.wrap.reacher.ik_solver import IKSolver, IKSolverConfig
 
 import numpy as np
 import pdb
 import toppra as ta
 import transforms3d as t3d
+
+from curobo.util import logger
+
 # import envs._GLOBAL_CONFIGS as CONFIGS
 
 class CuroboPlanner():
@@ -31,7 +37,22 @@ class CuroboPlanner():
         with open(self.yml_path, 'r') as f:
             yml_data = yaml.safe_load(f)
         self.frame_bias = yml_data['planner']['frame_bias']
-        
+        self.urdf_path = yml_data['robot_cfg']['kinematics']['urdf_path']
+        self.base_link = yml_data['robot_cfg']['kinematics']['base_link']
+        self.ee_link = yml_data['robot_cfg']['kinematics']['ee_link']
+        self.tensor_args = TensorDeviceType()
+            
+        robot_cfg = RobotConfig.from_basic(self.urdf_path, self.base_link, self.ee_link, self.tensor_args)
+        ik_config = IKSolverConfig.load_from_robot_config(
+                                            robot_cfg,
+                                            None,
+                                            num_seeds=20,
+                                            self_collision_check=True,
+                                            self_collision_opt=True,
+                                            tensor_args=self.tensor_args,
+                                            use_cuda_graph=True,
+                                        )
+        self.ik_solver = IKSolver(ik_config)
         # motion generation
         if True: # if not aloha
             world_config = {
@@ -42,24 +63,26 @@ class CuroboPlanner():
                     },
                 }
             }
+        
         motion_gen_config = MotionGenConfig.load_from_robot_config(
             self.yml_path,
-            world_config,
+            # world_config,
             interpolation_dt=1/250,
             num_trajopt_seeds=1
         )
-
         self.motion_gen = MotionGen(motion_gen_config)
         self.motion_gen.warmup()
-        motion_gen_config = MotionGenConfig.load_from_robot_config(
-            self.yml_path,
-            world_config,
-            interpolation_dt=1/250,
-            num_trajopt_seeds=1
-        )
         self.motion_gen_batch = MotionGen(motion_gen_config)
         # self.motion_gen_batch.warmup(batch=10) # batch=CONFIGS.ROTATE_NUM = 10
+        
     
+    def ik(self, target_gripper_pose):
+        goal_pose_of_gripper = CuroboPose.from_list(list(target_gripper_pose[:3]) + list(target_gripper_pose[-4:]))
+        result = self.ik_solver.solve_single(goal_pose_of_gripper)
+        # print(result.success)
+        # print(result.js_solution.position)
+        return result
+
     # target_gripper_pose np array [:7] x y z qw qx qy qz
     def plan_path(self, curr_joint_pos, target_gripper_pose, constraint_pose=None, arms_tag=None):
         
@@ -142,12 +165,16 @@ class CuroboPlanner():
         result_q = t3d.quaternions.mat2quat(wRb.T @ wRt)
         return result_p, result_q
 
-
-
 if __name__ == "__main__":
     
     planner = CuroboPlanner(active_joints_name=["fl_joint1","fl_joint2","fl_joint3","fl_joint4","fl_joint5","fl_joint6"], yml_path='/home/niantian/projects/aloha_maniskill_sim/curobo_left.yml')
     curr_joint_pos = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
     target_gripper_pose = np.array([0.0506, -0.0018,  0.3029, 0.7600,  0.4181, -0.4921,  0.0737])
-    result = planner.plan_path(curr_joint_pos, target_gripper_pose)
-    print(result)
+    import time
+    for i in range(10):
+        start  = time.time()
+        result = planner.plan_path(curr_joint_pos, target_gripper_pose)
+        # result = planner.ik(target_gripper_pose)
+        end = time.time()
+        print(f"time cost:{end - start}s")
+    # print(result)
--- planner/sim_robot.py
+++ planner/sim_robot.py
@@ -2,10 +2,14 @@ import sapien.core as sapien
 from sapien.utils.viewer import Viewer
 import numpy as np
 import cv2
-
+from pynput import keyboard
+import time
 import sys
 sys.path.append("./")
 from curobo_planner import CuroboPlanner
+
+x, y, z =0.0, 0.0, 0.0
+move_type = 0
 
 class ArmRobotController:
     def __init__(self, urdf_path: str, fix_root_link=True, balance_passive_force=True):
@@ -62,9 +66,12 @@ class ArmRobotController:
         self.right_wrist_camera = self._setup_camera()
         self.front_camera = self._setup_camera()
 
+    def set_planner(self, left_planner, right_planner):
+        self.left_planner = left_planner
+        self.right_planner = right_planner
+
     def _get_link_by_name(self, name: str):
         for link in self.robot.get_links():
-            print(link.get_name())
             if link.get_name() == name:
                 return link
         raise ValueError(f"Link '{name}' not found")
@@ -80,6 +87,10 @@ class ArmRobotController:
 
         self.right_wrist_camera.set_pose(right_wrist_pos)
 
+        front_camrea_pos_link = self._get_link_by_name(self.front_camera_link_name)
+        right_wrist_pos = front_camrea_pos_link.get_pose()
+        self.front_camera.set_pose(right_wrist_pos)
+
     def _setup_camera(self):
         near, far = 0.1, 100
         width, height, fovy = 640, 480, 37
@@ -91,10 +102,10 @@ class ArmRobotController:
             near=near,
             far=far
         )
-        # camera.set_pose(sapien.Pose(p=[0.05, 0, 0], q=[1, 0, 0, 0]))
         return camera
 
     def take_picture(self):
+        self._update_camera()
         self.left_wrist_camera.take_picture()
 
         # 获取 RGBA 图像，float32, (H, W, 4), [0, 1]
@@ -121,10 +132,12 @@ class ArmRobotController:
                 qpos[idx] = angle
         self.robot.set_qpos(qpos)
 
-    def run_trajectory(self, joint_indices, trajectory, steps_per_target=4):
+    def run_trajectory(self, joint_indices, trajectory, steps_per_target=1):
+        
         for target_angles in trajectory:
             qpos = self.robot.get_qpos()
             for idx, angle in zip(joint_indices, target_angles):
+                # import pdb;pdb.set_trace()
                 qpos[idx] = angle
             self.robot.set_qpos(qpos)
 
@@ -163,38 +176,47 @@ class ArmRobotController:
             self.scene.update_render()
             self.viewer.render()
 
-    def left_move(self,delta_move):
+    def left_move(self,delta_move,step_n=10):
         current_left_joint_pose = self.get_joint_positions(self.left_arm_indices)
         current_left_end_effort_pose = self.get_relative_pose(self.left_base_link_name, self.left_end_effort_name)
         current_left_end_effort_pose = np.concatenate([current_left_end_effort_pose.p, current_left_end_effort_pose.q])
-
+        
         left_target_gripper_pose = current_left_end_effort_pose + delta_move
-
+        
+        start_time = time.time()
         left_result = self.left_planner.plan_path(current_left_joint_pose, left_target_gripper_pose)
+        end_time = time.time()
+        print(f"单臂:{end_time - start_time}s")
 
-        if left_result['status'] == "Fail":
-            print("Plan Fail!")
-        else:
-            self.run_trajectory(self.left_arm_indices, left_result["position"])
+        if not np.array(left_result.success.cpu())[0][0]:
+            left_result = current_left_joint_pose
+            print("left ik fail")
+            return
+        left_result = np.array(left_result.js_solution.position)[0][0]
+        left_path = np.linspace(current_left_joint_pose, left_result, step_n)
+        self.run_trajectory(self.left_arm_indices, left_path)
     
-    def right_move(self,delta_move):
+    def right_move(self,delta_move,step_n=10):
         current_right_joint_pose = self.get_joint_positions(self.right_arm_indices)
         current_right_end_effort_pose = self.get_relative_pose(self.right_base_link_name, self.right_end_effort_name)
         current_right_end_effort_pose = np.concatenate([current_right_end_effort_pose.p, current_right_end_effort_pose.q])
 
         right_target_gripper_pose = current_right_end_effort_pose + delta_move
 
-        right_result = self.right_planner.plan_path(current_right_joint_pose, right_target_gripper_pose)
-
-        if right_result['status'] == "Fail":
-            print("Plan Fail!")
-        else:
-            self.run_trajectory(self.right_arm_indices, right_result["position"])
+        start_time = time.time()
+        right_result = self.right_planner.ik(right_target_gripper_pose)
+        end_time = time.time()
+        print(f"单臂:{end_time - start_time}s")
+        
+        if not np.array(right_result.success.cpu())[0][0]:
+            right_result = current_right_joint_pose
+            print("right ik fail")
+            return
+        right_result = np.array(right_result.js_solution.position)[0][0]
+        right_path = np.linspace(current_right_joint_pose, right_result, step_n)
+        self.run_trajectory(self.right_arm_indices, right_path)
     
-    '''
-    
-    '''
-    def move(self,left_delta_move, right_delta_move):
+    def move(self,left_delta_move, right_delta_move, step_n=30):
         current_left_joint_pose = self.get_joint_positions(self.left_arm_indices)
         current_left_end_effort_pose = self.get_relative_pose(self.left_base_link_name, self.left_end_effort_name)
         current_left_end_effort_pose = np.concatenate([current_left_end_effort_pose.p, current_left_end_effort_pose.q])
@@ -206,20 +228,33 @@ class ArmRobotController:
         current_right_end_effort_pose = np.concatenate([current_right_end_effort_pose.p, current_right_end_effort_pose.q])
 
         right_target_gripper_pose = current_right_end_effort_pose + right_delta_move
+        start_time = time.time()
 
-        left_result = self.left_planner.plan_path(current_left_joint_pose, left_target_gripper_pose)
-        right_result = self.right_planner.plan_path(current_right_joint_pose, right_target_gripper_pose)
+        left_result = self.left_planner.ik(left_target_gripper_pose)
+        right_result = self.right_planner.ik(right_target_gripper_pose)
 
-        left_path = left_result["position"]
-        right_path = right_result["position"]
+        if not np.array(left_result.success.cpu())[0][0]:
+            left_result = current_left_joint_pose
+            print("left ik fail")
+        else:
+            left_result = np.array(left_result.js_solution.position.cpu())[0][0]
+        
+        if not np.array(right_result.success.cpu())[0][0]:
+            right_result = current_right_joint_pose
+            print("right ik fail")
+        else:
+            right_result = np.array(right_result.js_solution.position.cpu())[0][0]
 
-        left_path, right_path = self.pad_to_same_length(left_path, right_path)
+        left_path = np.linspace(current_left_joint_pose, left_result, step_n)
+        right_path = np.linspace(current_right_joint_pose, right_result, step_n)
+
+        end_time = time.time()
+        print(f"双臂推理: {end_time - start_time}s")
+
         full_path = np.hstack([left_path, right_path])
 
-        if left_result['status'] == "Fail" or right_result['status'] == "Fail":
-            print("Plan Fail!")
-        else:
-            self.run_trajectory( (self.left_arm_indices + self.right_arm_indices) , full_path)
+        # _full = np.hstack([left_result, right_result])
+        self.run_trajectory((self.left_arm_indices + self.right_arm_indices) , full_path, steps_per_target=4)
 
     def pad_to_same_length(self, traj1, traj2):
         len1, len2 = len(traj1), len(traj2)
@@ -231,28 +266,100 @@ class ArmRobotController:
             traj2 = np.vstack([traj2, pad])
         return traj1, traj2
 
+def print_instructions():
+    """打印键盘控制说明"""
+    print("\n" + "="*50)
+    print("双臂运动控制系统 - 使用说明")
+    print("="*50)
+    print("位移控制:")
+    print("  W/S: 前/后移动(X轴)")
+    print("  A/D: 左/右移动(Y轴)")
+    print("  Q/E: 上/下移动(Z轴)")
+    print("\n模式选择:")
+    print("  0: 双同时运动")
+    print("  1: 仅左臂运动")
+    print("  2: 仅右臂运动")
+    print("\n其他:")
+    print("  ESC: 退出程序")
+    print("="*50 + "\n")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False  # 停止监听
+
+def on_press(key):
+    global x,y,z,move_type
+    x, y, z = 0.0, 0.0, 0.0
+    # delta_q = [0, 0, 0, 0]
+    # 位移控制
+    delta_move =0.01
+    if key.char == 'w': x += delta_move
+    elif key.char == 's': x -= delta_move
+    elif key.char == 'a': y += delta_move
+    elif key.char == 'd': y -= delta_move
+    elif key.char == 'q': z += delta_move
+    elif key.char == 'e': z -= delta_move
+    
+    # 模式选择
+    elif key.char == '0': move_type = 0
+    elif key.char == '1': move_type = 1
+    elif key.char == '2': move_type = 2
+
 def main():
     controller = ArmRobotController(
         urdf_path="/home/niantian/projects/aloha_maniskill_sim/urdf/arx5_description_isaac.urdf",
         fix_root_link=True,
         balance_passive_force=True
     )
+    left_planner = CuroboPlanner(active_joints_name=["fl_joint1","fl_joint2","fl_joint3","fl_joint4","fl_joint5","fl_joint6"],\
+                                yml_path='/home/niantian/projects/aloha_maniskill_sim/curobo_left.yml')
+    
+    right_planner = CuroboPlanner(active_joints_name=["fr_joint1","fr_joint2","fr_joint3","fr_joint4","fr_joint5","fr_joint6"],\
+                            yml_path='/home/niantian/projects/aloha_maniskill_sim/curobo_right.yml')
 
+    controller.set_planner(left_planner, right_planner)
+
+    # 获取图像
     rgb, depth = controller.take_picture()
     cv2.imwrite("rgb.jpg", rgb)
-    # cv2.imshow("depth", depth)
-    # cv2.waitKey(0.01)
+    cv2.imwrite("depth.jpg",depth)
     
     # 设置初始角度
     controller.set_arm_qpos(left_angles=[0.0]*6, right_angles=[0.0]*6)
+    
+    # 运动示例:
     # 单臂运动
-    left_move = np.array([0.05, 0.05, 0.1, 0, 0, 0, 0])
-    controller.left_move(left_move)
+    # left_move = np.array([0.05, 0.05, 0.1, 0, 0, 0, 0])
+    # controller.left_move(left_move)
     # 双臂一同运动
     left_move = np.array([0, -0.05, 0.1, 0, 0, 0, 0])
     right_move = np.array([0.10, -0.05, 0.1, 0, 0, 0, 0])
+    
     controller.move(left_move, right_move)
+    
+    
+    '''
+    0: 左臂右臂一同运动
+    1: 左臂
+    2:右臂
+    '''
+    print_instructions()
 
+    global x,y,z,move_type
+
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+    
+    while listener.is_alive():
+        while True:
+            delata_move = np.array([x, y, z] + [0, 0, 0, 0])
+            if move_type == 0:
+                controller.move(delata_move,delata_move)
+            elif move_type == 1:
+                controller.left_move(delata_move)
+            elif move_type == 2:
+                controller.right_move(delata_move)
+    
     # 进入控制循环（可注释掉）
     controller.loop()
 
