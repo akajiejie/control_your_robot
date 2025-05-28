cb1858224786646dfd76f7fd9028640ec632b29b
Tian-Nian <1250576969@qq.com>
2025-05-29 04:13:06 +0800

    modify pika_controller, debuging now...

--- controller/Pika_controller.py
+++ controller/Pika_controller.py
@@ -5,9 +5,10 @@ import numpy as np
 
 from controller.teleoperation_controller import TeleoperationController
 from utils.ros_subscriber import ROSSubscriber 
+from utils.data_handler import compute_local_delta_pose
 
 from geometry_msgs.msg import PoseStamped
-from sensor_tools import Gripper
+# from sensor_tools import Gripper
 from scipy.spatial.transform import Rotation as R
 from typing import Callable, Optional
 
@@ -24,19 +25,23 @@ class PikaController(TeleoperationController):
         self.controller = None
     
     def set_up(self, pos_node_name, gripper_node_name, call: Optional[Callable] = None):
-        if call:
-            pos_subscriber = ROSSubscriber(pos_node_name, PoseStamped, call)
-        else:
-            gripper_subscriber = ROSSubscriber(gripper_node_name, Gripper)
+
+        self.pos_subscriber = ROSSubscriber(pos_node_name, PoseStamped, call)
+
+        # self.gripper_subscriber = ROSSubscriber(gripper_node_name, Gripper)
+        
         self.controller = {
-            "pos_subscriber":pos_subscriber,
-            "gripper_subscriber":gripper_subscriber,
+            "pos_subscriber":self.pos_subscriber,
+            # "gripper_subscriber":self.gripper_subscriber,
         }
 
     def get_state(self):
         pos_msg = self.controller["pos_subscriber"].get_latest_data()
+        if pos_msg is None:
+            return None
+        
         roll, pitch, yaw = R.from_quat([pos_msg.pose.orientation.x,pos_msg.pose.orientation.y, \
-                                        pos_msg.pose.orientation.z,pos_msg.pose.orientation.w])
+                                        pos_msg.pose.orientation.z,pos_msg.pose.orientation.w]).as_euler('xyz')
         qpos = np.array([pos_msg.pose.position.x,
                 pos_msg.pose.position.y,
                 pos_msg.pose.position.z,
@@ -44,24 +49,25 @@ class PikaController(TeleoperationController):
                 pitch,
                 yaw,])
         
-        gripper_msg = self.controller["gripper_subscriber"].get_latest_data()
+        # gripper_msg = self.controller["gripper_subscriber"].get_latest_data()
         # 归一化
-        gripper = (np.array([gripper_msg.joint_states]) - 0.3) / 1.7
+        # gripper = (np.array([gripper_msg.joint_states]) - 0.3) / 1.7
         
         return {
             "end_pose":qpos,
-            "gripper":gripper
+            # "gripper":gripper
         }
 
 if __name__ == "__main__":
     import time
+    import rospy
     pika_left = PikaController("left_pika")
-    pika_right = PikaController("right_pika")
+    # pika_right = PikaController("right_pika")
 
-    pika_left.set_up("/gripper_pose_l","/gripper_l")
-    pika_left.set_up("/gripper_pose_r","/gripper_r")
-
+    pika_left.set_up("/pika_pose_l","/gripper_l")
+    # pika_left.set_up("/gripper_r","/gripper_r")
+    rospy.init_node('ros_subscriber_node', anonymous=True)
     while True:
         print("left_pika:\n", pika_left.get_state())
-        print("right_pika:\n", pika_right.get_state())
+        # print("right_pika:\n", pika_right.get_state())
         time.sleep(0.1)
--- utils/data_handler.py
+++ utils/data_handler.py
@@ -7,6 +7,45 @@ import fnmatch
 import sys
 import select
 
+from scipy.spatial.transform import Rotation
+
+
+def compute_rotate_matrix(pose):
+    """将位姿 [x,y,z,roll,pitch,yaw] 转换为齐次变换矩阵 (XYZ欧拉角顺序)"""
+    x, y, z, roll, pitch, yaw = pose
+
+    R = Rotation.from_euler('XYZ', [roll, pitch, yaw]).as_matrix()
+    
+    T = np.eye(4)
+    T[:3, :3] = R
+    T[:3, 3] = [x, y, z]
+    
+    return T
+
+def compute_local_delta_pose(base_pose, target_pose):
+    """
+    计算局部坐标系下的位姿增量 (基于base_pose的坐标系)
+    参数:
+        base_pose: 基准位姿 [x,y,z,roll,pitch,yaw]
+        target_pose: 目标位姿 [x,y,z,roll,pitch,yaw]
+    返回:
+        增量位姿 [delta_x, delta_y, delta_z, delta_roll, delta_pitch, delta_yaw]
+    """
+    assert len(base_pose) == 6 and len(target_pose) == 6, "输入位姿必须是6维"
+    
+    # 计算旋转增量
+    base_rotate = Rotation.from_euler('XYZ', base_pose[3:])
+    target_rotate = Rotation.from_euler('XYZ', target_pose[3:])
+    delta_rotate = base_rotate.inv() * target_rotate
+    delta_rpy = delta_rotate.as_euler('XYZ', degrees=False)
+    
+    # 计算平移增量（转换到局部坐标系）
+    delta_global = np.array(target_pose[:3]) - np.array(base_pose[:3])
+    delta_xyz = base_rotate.inv().apply(delta_global)
+    
+    return np.concatenate([delta_xyz, delta_rpy])
+
+
 def get_item(Dict_data: Dict, item):
     if isinstance(item, str):
         keys = item.split(".")
@@ -87,3 +126,11 @@ def debug_print(name, info, level="INFO"):
 
 def is_enter_pressed():
     return select.select([sys.stdin], [], [], 0)[0] and sys.stdin.read(1) == '\n'    
+
+if __name__=="__main__":
+    s = np.array([0.1, 0.2, 0.3, 0., 0., 0.1])
+    print(compute_rotate_matrix(s))
+
+    base_pose = np.array([0, 0, 0, 0, 0, 1])
+    target_pose = np.array([1, 1, 1, 1, 0, 0])
+    print(compute_local_delta_pose(base_pose, target_pose))
