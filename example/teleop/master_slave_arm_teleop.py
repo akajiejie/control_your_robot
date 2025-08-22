a9b6bda8d678e6ab8d56ff0092a55540af556cc5
Tian-Nian <1250576969@qq.com>
2025-08-22 15:10:13 +0800

    fix bug

--- example/teleop/master_slave_arm_teleop.py
+++ example/teleop/master_slave_arm_teleop.py
@@ -16,7 +16,7 @@ condition = {
     "save_path": "./save/", 
     "task_name": "test_3", 
     "save_format": "hdf5", 
-    "save_freq": 10,
+    "save_freq": 30,
     "collect_type": "teleop",
 }
 
@@ -102,7 +102,7 @@ if __name__ == "__main__":
         slave = SlaveWorker("slave_arm", start_event, end_event, master.data_buffer)
         data = DataWorker("collect_data", start_event, end_event, slave.data_buffer, episode_id=i)
 
-        time_scheduler = TimeScheduler([master.forward_event], time_freq=30, end_Events=[data.next_event])
+        time_scheduler = TimeScheduler(work_events=[master.forward_event], time_freq=30, end_events=[data.next_event])
         
         master.next_to(slave)
         slave.next_to(data)
